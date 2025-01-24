942018bd261ac3f08746327d6beab2e394ce66ee
Aryan Agarwal <123481236+aryan-0302@users.noreply.github.com>
2025-01-24 18:22:01 +0000

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   import.meta.env.MODE === 'development'
-    ? 'http://localhost:4000'     : import.meta.env.VITE_BASE_URL || '';
+    ? 'http://localhost:4000'     : import.meta.env.VITE_BASE_URL || 'https://study-notion-fullstack-jade.vercel.app/';
 
 const endpoints = {
     SENDOTP_API: BASE_URL + "/auth/sendotp",
